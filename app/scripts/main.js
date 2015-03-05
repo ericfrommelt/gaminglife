226ee0b72cd1872f6afb1aaf2671c6b88cf76aff
ericfrommelt <ericfrommelt@gmail.com>
2015-03-04 20:18:36 -0500

    setup json and ran initial test with d3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+var data;
+
+d3.json("../data/data.json", function(error, json) {
+  data = json;
+  if (error) {
+    console.log("error");
+  } else {
+    console.log(data);
+    generateDiv(data);
+  }
+});
+
+function generateDiv(data) {
+  d3.select("#graph").selectAll("div")
+  .data(data.games)
+  .enter()
+  .append("div")
+  .style("width", function(d) {
+    var barWidth = d.hours * 2;
+    return barWidth + "px";
+  });
+  console.log("called");
+}
